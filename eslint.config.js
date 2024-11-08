3e5a442c4255a1ee01d86213705ddda15dc81db4
Paulo Bandua <pcbandua@gmail.com>
2024-11-07 16:34:08 -0800

    Header, footer, ApprenticeshipsPage Created

--- eslint.config.js
+++ eslint.config.js
@@ -5,9 +5,9 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
-  { ignores: ['dist'] },
   {
     files: ['**/*.{js,jsx}'],
+    ignores: ['dist'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -29,6 +29,7 @@ export default [
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
       'react/jsx-no-target-blank': 'off',
++       'react/prop-types': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
---
+++ src/ApprenticeshipsPage.jsx
@@ -0,0 +1,7 @@
+export function ApprenticeshipsPage() {
+  return (
+    <main>
+      <h1>Get Paid While You Learn</h1>
+    </main>
+  )
+}
